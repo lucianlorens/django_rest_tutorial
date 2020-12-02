2559838a9a9c093d821c8e9b27de318c307fe9ca
lucianlorens <lucianlorens@gmail.com>
2020-12-02 04:04:49 -0300

    add gitignore, views and serializers

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allow users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
